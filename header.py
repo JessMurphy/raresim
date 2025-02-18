dbdaa9466728aa1b4b7a14fa62a40857f2b831be
RMBarnard <rbarnard1107@gmail.com>
2025-02-18 10:24:16 -0700

    Switch pooling implementation to only pull from common variants when necessary

--- header.py
+++ header.py
@@ -108,7 +108,7 @@ def prune_bins(bin_assignments, bins, extra_rows, matrix, activation_threshold, 
     @param extra_rows: list of rows that were removed and never re-added
     @param matrix: sparse matrix of haps
     '''
-    extra_rows += bin_assignments[len(bin_assignments)-1]
+    reserve_pool = bin_assignments[len(bin_assignments)-1]
     # Loop through the bins from largest to smallest
     for bin_id in range(len(bin_assignments)-1)[::-1]:
         # If there are any rows with too many 1s for the largest bin, they get put into an extra bin,
@@ -131,6 +131,7 @@ def prune_bins(bin_assignments, bins, extra_rows, matrix, activation_threshold, 
             for i in range(have):
                 # If we hit our stopping threshold to stop the pruning process, then stop the pruning process
                 if have - len(row_ids_to_rem) <= need - stop:
+                    print (f"Stopping threshold reaching for bin {bin_id}")
                     break
                 flip = random.uniform(0, 1)
                 # If the row is 'chosen' for removal, remove it and add the row to the list of rows that may be used
@@ -146,35 +147,53 @@ def prune_bins(bin_assignments, bins, extra_rows, matrix, activation_threshold, 
 
         # If we don't have enough rows in the current bin, pull from the list of excess rows
         elif have < round(need - activation):
+            pullFromCommons = False
             if len(extra_rows) < round(abs(need - have)):
-                raise Exception(f'ERROR: Current bin has {have} variants, but the model needs {need} variants '
-                                f'and only {len(extra_rows)} excess rows are available to use and prune down '
-                                f'from larger bins.')
+                if len(reserve_pool) < round(abs(need-have)):
+                    raise Exception(f'ERROR: Current bin has {have} variants, but the model needs {need} variants '
+                                    f'and only {len(extra_rows)} excess rows are available to use and prune down '
+                                    f'from larger bins and only {len(reserve_pool)} common variants are available '
+                                    f'to prune down.')
+                else:
+                    print(f"WARNING: No more extra pruned rows to pull from. Common variants may be pruned down for bin {bin_id+1} if necessary")
+                    pullFromCommons = True
 
             # Calculate the probability to use any given row from the available list
-            if len(extra_rows) == 0:
-                print ("No more extra pruned rows to pull from, skipping")
-                continue;
-            prob_add = float(need - have) / float(len(extra_rows))
-            row_ids_to_add = []
+            prob_add_from_extras = float(need - have) / float(len(extra_rows)) if len(extra_rows) > 0 else 1
+            prob_add_from_reserve = float(need - have - len(extra_rows)) / float(len(reserve_pool))
+            row_ids_to_add_from_extras = []
             for i in range(len(extra_rows)):
                 flip = random.uniform(0, 1)
                 # If the current row is 'chosen' for use, we will prune it down to the desired number of variants
                 # and add it back in to the current bin and remove it from the list of available rows to pull from
-                if flip < prob_add:
+                if flip < prob_add_from_extras:
                     row_id = extra_rows[i]
-                    row_ids_to_add.append(row_id)
+                    row_ids_to_add_from_extras.append(row_id)
                     num_to_keep = random.randint(bins[bin_id][0], bins[bin_id][1])
                     num_to_rem = matrix.row_num(row_id) - num_to_keep
                     matrix.prune_row(row_id, num_to_rem)
                     if matrix.row_num(row_id) != num_to_keep:
                         print(f"WARNING: Requested to prune row {row_id} down to {num_to_keep} variants, but after the request the row still has {matrix.row_num} variants. This should not happen and the issue likely needs to be raised with a developer.")
                     bin_assignments[bin_id].append(row_id)
-                    if row_id in bin_assignments[len(bin_assignments) - 1]:
-                        bin_assignments[len(bin_assignments) - 1].remove(row_id)
 
-            for row_id in row_ids_to_add:
+            for row_id in row_ids_to_add_from_extras:
                 extra_rows.remove(row_id)
+
+            if pullFromCommons:
+                row_ids_to_add_from_reserve = []
+                for i in range(len(reserve_pool)):
+                    flip = random.uniform(0, 1)
+                    if flip < prob_add_from_reserve:
+                        row_id = reserve_pool[i]
+                        row_ids_to_add_from_reserve.append(row_id)
+                        num_to_keep = random.randint(bins[bin_id][0], bins[bin_id][1])
+                        num_to_rem = matrix.row_num(row_id) - num_to_keep
+                        matrix.prune_row(row_id, num_to_rem)
+                        if matrix.row_num(row_id) != num_to_keep:
+                            print(f"WARNING: Requested to prune row {row_id} down to {num_to_keep} variants, but after the request the row still has {matrix.row_num} variants. This should not happen and the issue likely needs to be raised with a developer.")
+                        bin_assignments[bin_id].append(row_id)
+                for row_id in row_ids_to_add_from_reserve:
+                    reserve_pool.remove(row_id)
 
 def print_bin(bin_assignments, bins):
     print(f"{'Bin':<12}{'Expected':<18}{'Actual':<10}")
